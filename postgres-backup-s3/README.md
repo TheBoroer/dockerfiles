c8beb7e9e630f9c35aa62398f577622413d71b4f
Boro Vukovic <me@bo.ro>
2025-02-19 01:22:48 -0500

    postgres: add option to backup all found databases

--- postgres-backup-s3/README.md
+++ postgres-backup-s3/README.md
@@ -5,11 +5,13 @@ Backup PostgresSQL to S3 (supports periodic backups)
 ## Usage
 
 Docker:
+
 ```sh
 $ docker run -e S3_ACCESS_KEY_ID=key -e S3_SECRET_ACCESS_KEY=secret -e S3_BUCKET=my-bucket -e S3_PREFIX=backup -e POSTGRES_DATABASE=dbname -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_HOST=localhost boro/postgres-backup-s3
 ```
 
 Docker Compose:
+
 ```yaml
 postgres:
   image: postgres
@@ -22,17 +24,25 @@ pgbackups3:
   links:
     - postgres
   environment:
-    SCHEDULE: '@daily'
-    S3_REGION: region
+    SCHEDULE: "@daily"
+    S3_ENDPOINT: region
     S3_ACCESS_KEY_ID: key
     S3_SECRET_ACCESS_KEY: secret
     S3_BUCKET: my-bucket
-    S3_PREFIX: backup
+    S3_PREFIX: daily/backup
     POSTGRES_DATABASE: dbname
+    POSTGRES_HOST: postgres
+    POSTGRES_PORT: 5432
     POSTGRES_USER: user
     POSTGRES_PASSWORD: password
-    POSTGRES_EXTRA_OPTS: '--schema=public --blobs'
+    POSTGRES_EXTRA_OPTS: "--schema=public --blobs"
 ```
+
+### If `POSTGRES_DATABASE` is not set
+
+It will attempt to use the login credentials to find & backup all databases into a single .sql.gz file.
+
+If `MULTI_FILES` is set to `yes`, it'll create a separate gzip backup for each database + a gzip for postgres globals.
 
 ### Automatic Periodic Backups
 
@@ -40,3 +50,23 @@ You can additionally set the `SCHEDULE` environment variable like `-e SCHEDULE="
 
 More information about the scheduling can be found [here](http://godoc.org/github.com/robfig/cron#hdr-Predefined_schedules).
 
+## Environment Variables
+
+The following environment variables can be set to configure the behavior of the `postgres-backup-s3` container:
+
+- **S3_ACCESS_KEY_ID**: Your AWS access key ID. This is required to authenticate with AWS S3.
+- **S3_SECRET_ACCESS_KEY**: Your AWS secret access key. This is required to authenticate with AWS S3.
+- **S3_BUCKET**: The name of the S3 bucket where backups will be stored. This is required.
+- **S3_REGION**: The AWS region where your S3 bucket is located. Defaults to `us-west-1`.
+- **S3_PREFIX**: The prefix (or folder path) within the S3 bucket where backups will be stored. Defaults to `backup`.
+- **S3_ENDPOINT**: Custom S3 endpoint URL, useful for S3-compatible services. Defaults to `None`.
+- **POSTGRES_DATABASE**: The name of the PostgreSQL database to back up. If not set, all databases will be backed up.
+- **POSTGRES_HOST**: The hostname or IP address of the PostgreSQL server. This is required.
+- **POSTGRES_PORT**: The port number on which the PostgreSQL server is listening. Defaults to `5432`.
+- **POSTGRES_USER**: The PostgreSQL user to connect as. This is required.
+- **POSTGRES_PASSWORD**: The password for the PostgreSQL user. This is required.
+- **POSTGRES_EXTRA_OPTS**: Additional options to pass to the `pg_dump` or `pg_dumpall` command.
+- **SCHEDULE**: A cron expression defining the schedule for automatic backups. If not set, backups must be triggered manually.
+- **MULTI_FILES**: If set to `yes`, each database will be backed up into a separate file, along with a separate file for PostgreSQL globals.
+
+These environment variables allow you to customize the backup process to suit your needs, including specifying which databases to back up, where to store the backups, and how often to perform the backups automatically.
--- postgres-backup-s3/backup.sh
+++ postgres-backup-s3/backup.sh
@@ -3,6 +3,29 @@
 set -e
 set -o pipefail
 
+copy_s3() {
+  SRC_FILE=$1
+  DEST_FILE=$2
+
+  if [ "${S3_ENDPOINT}" == "**None**" ]; then
+    AWS_ARGS=""
+  else
+    AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
+  fi
+
+  echo "Uploading $SRC_FILE to $S3_ENDPOINT/$S3_BUCKET/$S3_PREFIX$DEST_FILE"
+
+  #cat $SRC_FILE | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE || exit 2
+  aws $AWS_ARGS s3api put-object --body $SRC_FILE --bucket $S3_BUCKET --key $S3_PREFIX$DEST_FILE || exit 2
+
+  if [ $? != 0 ]; then
+    echo >&2 "Error uploading ${DEST_FILE} on S3"
+  fi
+
+  rm $SRC_FILE
+}
+
+# Check for required env vars
 if [ "${S3_ACCESS_KEY_ID}" = "**None**" ]; then
   echo "You need to set the S3_ACCESS_KEY_ID environment variable."
   exit 1
@@ -33,15 +56,19 @@ if [ "${POSTGRES_PASSWORD}" = "**None**" ]; then
   exit 1
 fi
 
-if [ "${POSTGRES_DATABASE}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
-  exit 1
-fi
-
 if [ "${S3_ENDPOINT}" == "**None**" ]; then
   AWS_ARGS=""
 else
   AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
+fi
+
+# Set default values
+if [ -z "${POSTGRES_PORT}" ]; then
+  POSTGRES_PORT=5432
+fi
+
+if [ "${POSTGRES_DATABASE}" = "**None**" ]; then
+  echo "POSTGRES_DATABASE environment variable isn't set, defaulting to backing up all databases."
 fi
 
 # env vars needed for aws tools
@@ -51,14 +78,39 @@ export AWS_DEFAULT_REGION=$S3_REGION
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
+DUMP_START_TIME=$(date +"%Y-%m-%dT%H:%M:%SZ")
 
-echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
+if [ "${POSTGRES_DATABASE}" != "**None**" ]; then
+  # Backup single specified database
+  BACKUP_FILENAME="${POSTGRES_DATABASE}_${DUMP_START_TIME}.sql.gz"
+  echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST} to ${BACKUP_FILENAME}..."
+  pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip >"${BACKUP_FILENAME}"
+  copy_s3 $BACKUP_FILENAME $BACKUP_FILENAME
+else
+  # Backup all databases
 
-pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
+  # Multi file: yes
+  if [ ! -z "$(echo $MULTI_FILES | grep -i -E "(yes|true|1)")" ]; then
+    # backup globals
+    BACKUP_FILENAME="globals_${DUMP_START_TIME}.sql.gz"
+    echo "Creating dump of globals from ${POSTGRES_HOST}:${POSTGRES_PORT}..."
+    pg_dumpall $POSTGRES_HOST_OPTS --globals-only | gzip >"${BACKUP_FILENAME}"
+    copy_s3 $BACKUP_FILENAME $BACKUP_FILENAME
 
-echo "Uploading dump to $S3_BUCKET"
+    # backup each database into a separate file
+    for DB in $(psql $POSTGRES_HOST_OPTS -l -t | cut -d'|' -f1 | sed -e 's/ //g' -e '/^$/d' | grep -v -E "(template0|template1)"); do
+      BACKUP_FILENAME="${DB}_${DUMP_START_TIME}.sql.gz"
+      echo "Creating dump of ${DB} from ${POSTGRES_HOST}:${POSTGRES_PORT}..."
+      pg_dump $POSTGRES_HOST_OPTS --create $DB | gzip >"${BACKUP_FILENAME}"
+      copy_s3 $BACKUP_FILENAME $BACKUP_FILENAME
+    done
+  else
+    # backup all databases into a single dump file
+    BACKUP_FILENAME="${POSTGRES_HOST}_${POSTGRES_PORT}_${DUMP_START_TIME}.sql.gz"
+    echo "Creating dump of all databases from ${POSTGRES_HOST}:${POSTGRES_PORT}..."
+    pg_dumpall $POSTGRES_HOST_OPTS --clean --if-exists | gzip >"${BACKUP_FILENAME}"
+    copy_s3 $BACKUP_FILENAME $BACKUP_FILENAME
+  fi
+fi
 
-#cat dump.sql.gz | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
-aws $AWS_ARGS s3api put-object --body dump.sql.gz --bucket $S3_BUCKET --key $S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
-
-echo "SQL backup uploaded successfully"
+echo "Backup complete."
